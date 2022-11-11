54a09fb3bfe9cde96ad40c37c3aecdf9d31bacc9
DZH-ua <dzh.3743@gmail.com>
2022-11-11 15:27:13 +0200

    video update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const CURRENTTIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(CURRENTTIME_KEY, seconds);
 }
 
-console.log(localStorage.getItem('videoplayer-current-time'));
+console.log(localStorage.getItem(CURRENTTIME_KEY));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem(CURRENTTIME_KEY));
