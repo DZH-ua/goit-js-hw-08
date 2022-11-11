98de1888ba12718dc29db81e316a5389f0b1152f
DZH-ua <dzh.3743@gmail.com>
2022-11-11 10:01:44 +0200

    feedback update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+onFormLabelCheck();
+
+function onFormInput(event) {
+  event.preventDefault();
+
+  const feedback = {};
+
+  const formData = new FormData(form);
+  formData.forEach((value, name) => {
+    feedback[name] = value;
+    console.log(feedback);
+  });
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedback));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  console.log('очистили хранилище');
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onFormLabelCheck() {
+  const savedFormInfo = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedFormInfo = JSON.parse(savedFormInfo);
+
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    console.log('не пусто');
+    form.elements.message.value = parsedFormInfo.message;
+    form.elements.email.value = parsedFormInfo.email;
+  } else {
+    console.log('пусто');
+  }
+}
